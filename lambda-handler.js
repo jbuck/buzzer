5ec6f8c4c10dfd35ff2f276f7bf2c31c1e831b0a
Jon Buckley <jbuckley@mozilla.com>
2017-02-19 19:30:31 -0500

    Add AWS Lambda handler

---
+++ lambda-handler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const twilio = require("twilio");
+
+const OWNER_NUMBERS = process.env.OWNER_NUMBERS.split(",");
+
+module.exports.buzzer = (event, context, callback) => {
+  const twiml = new twilio.TwimlResponse();
+  twiml
+    .play({digits: "www9www99999www99999"})
+    .sms("Someone buzzed in!", {to: OWNER_NUMBERS});
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/xml"
+    },
+    body: JSON.stringify(twiml),
+  };
+
+  callback(null, response);
+};
